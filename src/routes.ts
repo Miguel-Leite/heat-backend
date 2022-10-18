0c6a4c4060dd29fa3444cf421a560225a096ac69
Miguel-Leite <miguelleite200leite@email.com>
2022-10-18 08:50:52 -0700

    create message

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
+import { CreateMessageController } from './controllers/CreateMessageController';
+import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
 
 const router = Router();
 
-const authenticateUserController = new AuthenticateUserController();
+const authenticateUserController    = new AuthenticateUserController();
+const createMessageController       = new CreateMessageController();
 
 router.post("/authenticate", authenticateUserController.handle);
+
+router.post("/messages", ensureAuthenticated , createMessageController.handle)
 
 export { router }
